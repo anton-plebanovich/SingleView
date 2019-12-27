1b810f8bd66d6c9fcac8cb583ed8d4647e4d3b68
Anton Plebanovich <anton.plebanovich@gmail.com>
2019-12-27 09:23:29 +0300

    - Stroke label bug

---
+++ Podfile
@@ -0,0 +1,31 @@
+# Deployment Target
+platform :ios, '10.3'
+
+# Ignore pods code warnings
+inhibit_all_warnings!
+
+# Add pods as frameworks so we could add obj-c and swift 3.0 pods
+use_frameworks!
+
+
+def core_pods
+#    pod 'ActionPickerUtils', :git => 'https://github.com/APUtils/ActionPickerUtils'
+    pod 'APExtensions', :git => 'https://github.com/APUtils/APExtensions'
+#    pod 'BaseClasses', :git => 'https://github.com/APUtils/BaseClasses'
+#    pod 'KeyboardAvoidingView'
+
+#    pod 'RxSwift', '~> 4.0'
+#    pod 'RxCocoa', '~> 4.0'
+end
+
+target 'SingleView' do
+    core_pods
+end
+
+
+post_install do |installer|
+    # Add podInstall.command and podUpdate.command shell scripts to Pods project
+    pods_project = installer.pods_project
+    pods_project.new_file "../podInstall.command"
+    pods_project.new_file "../podUpdate.command"
+end
--- SingleView/ViewController.swift
+++ SingleView/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.attributedText = getAttributedName("Label should be stroked", buyProgress: 1)
+        label.text = nil
+        label.attributedText = getAttributedName("Label shouldn't be stroked", buyProgress: 0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func getAttributedName(_ name: String, buyProgress: Double) -> NSAttributedString {
+        let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: name)
+        
+        let fullRange = NSRange(location: 0, length: attributeString.length)
+        attributeString.addAttribute(NSAttributedString.Key.baselineOffset, value: 0, range: fullRange)
+        
+        let buyLength = Int(Double(attributeString.length) * buyProgress)
+        let buyRange = NSRange(location: 0, length: buyLength)
+        attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: buyRange)
+        
+        return attributeString
     }
-
-
 }
 
