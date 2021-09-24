327674397f998a1d668c56afd6322d4409f5f9d8
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-09-25 02:11:18 +0300

    - Realm background queue observe bug

--- SingleView/Home/HomeVC.swift
+++ SingleView/Home/HomeVC.swift
@@ -8,10 +8,6 @@
 
 import APExtensions
 import RealmSwift
-import RxCocoa
-import RxRelay
-import RxSwift
-import RxUtils
 import UIKit
 
 final class HomeVC: UIViewController, InstantiatableFromStoryboard {
@@ -25,6 +21,7 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
     
     fileprivate let realmQueue = DispatchQueue(label: "realm", qos: .userInteractive)
     fileprivate let computeQueue = DispatchQueue(label: "compute", qos: .userInteractive)
+    fileprivate let app = App(id: "realmsandbox-grpmo")
     fileprivate var realm: Realm!
     fileprivate var token: NotificationToken!
     
@@ -33,49 +30,99 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        realmQueue.async { [self] in
-            realm = try! Realm(queue: realmQueue)
-            
-            let myObject = MyObject()
-            try! realm.write {
-                realm.deleteAll()
-                realm.add(myObject)
+        app.syncManager.logger = { logLevel, message in
+            switch logLevel {
+            case .fatal: print("SyncManager fatal error: \(message)")
+            case .error: print("SyncManager error: \(message)")
+            case .warn: print("[Warn] \(message)")
+            case .info: print("[Info] \(message)")
+            case .detail: print("[Detail] \(message)")
+            case .debug: print("[Debug] \(message)")
+            case .trace: print("[Trace] \(message)")
+            case .off: print("[Off] \(message)")
+            case .all: print("[All] \(message)")
             }
-            
-            token = realm.objects(MyObject.self).observe(on: realmQueue, { changeset in
-                switch changeset {
-                    
-                case .initial(let results):
-                    let text = results.first!.text
-                    DispatchQueue.main.async {
-                        realmLabel.text = text
+        }
+        
+        app.syncManager.logLevel = .all
+        
+        app.login(credentials: .anonymous) { [self] result in
+            switch result {
+            case .failure(let error):
+                print("Realm login failed: \(error)")
+                
+            case .success(let user):
+                print("Realm login success with user ID '\(user.id)'")
+                
+                var configuration = user.configuration(partitionValue: "P")
+                configuration.objectTypes = [
+                    TextRealmModel.self
+                ]
+                
+                Realm.asyncOpen(configuration: configuration, callbackQueue: realmQueue) { result in
+                    switch result {
+                    case .failure(let error):
+                        print("Failed to open realm: \(error)")
+                        
+                    case .success(let realm):
+                        print("Realm async open success")
+                        self.realm = realm
+                        
+                        realmQueue.async { [self] in
+                            token = realm.objects(TextRealmModel.self).observe(on: realmQueue, { changeset in
+                                switch changeset {
+                                    
+                                case .initial(let results):
+                                    let text = results.first!.text
+                                    print("Initial text set: \(text)")
+                                    DispatchQueue.main.async {
+                                        textLabel.text = "Local: \(text)"
+                                        realmLabel.text = "Realm: \(text)"
+                                    }
+                                    
+                                case .update(let results, _, _, _):
+                                    let text = results.first!.text
+                                    print("Update text set: \(text)")
+                                    DispatchQueue.main.async {
+                                        realmLabel.text = "Realm: \(text)"
+                                    }
+                                    
+                                case .error(let error):
+                                    print("Realm observe error: \(error)")
+                                }
+                            })
+                        }
                     }
-                    
-                case .update(let results, _, _, _):
-                    let text = results.first!.text
-                    DispatchQueue.main.async {
-                        realmLabel.text = text
-                    }
-                    
-                case .error(_):
-                    break
                 }
-            })
+            }
         }
     }
     
     @IBAction fileprivate func onGenerateTap(_ sender: Any) {
         let text = String.random(length: 10)
-        textLabel.text = text
+        textLabel.text = "Local: \(text)"
+        let value = AnyBSON(text)
         
-        realmQueue.async { [self] in
-            try! realm.write {
-                realm.objects(MyObject.self).first!.text = text
+        app.currentUser?.functions.updateTexts([value]) { result, error in
+            if let error = error {
+                print("Function call error: \(error)")
+            } else {
+                print("Function call success")
             }
+        }
+    }
+    
+    @IBAction fileprivate func onForceRefreshTap(_ sender: Any) {
+        realmQueue.async { [self] in
+            realm.refresh()
         }
     }
 }
 
-final class MyObject: Object {
+final class TextRealmModel: Object {
+    @objc dynamic var _id = ObjectId.generate()
+    @objc dynamic var _p = "P"
     @objc dynamic var text: String = ""
+    
+    override static func primaryKey() -> String? { "_id" }
 }
