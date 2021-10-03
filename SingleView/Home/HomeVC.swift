827499caf6a65aa2821c60915ac68e94060344ab
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-10-03 17:33:55 +0300

    - Duplicate check

--- SingleView/Home/HomeVC.swift
+++ SingleView/Home/HomeVC.swift
@@ -19,103 +19,28 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
     
     // ******************************* MARK: - Private Properties
     
-    fileprivate let realmQueue = DispatchQueue(label: "realm", qos: .userInteractive)
-    fileprivate let computeQueue = DispatchQueue(label: "compute", qos: .userInteractive)
-    fileprivate let app = App(id: "realmsandbox-grpmo")
-    fileprivate var realm: Realm!
-    fileprivate var token: NotificationToken!
-    
     // ******************************* MARK: - Initialization and Setup
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        app.syncManager.logger = { logLevel, message in
-            switch logLevel {
-            case .fatal: print("SyncManager fatal error: \(message)")
-            case .error: print("SyncManager error: \(message)")
-            case .warn: print("[Warn] \(message)")
-            case .info: print("[Info] \(message)")
-            case .detail: print("[Detail] \(message)")
-            case .debug: print("[Debug] \(message)")
-            case .trace: print("[Trace] \(message)")
-            case .off: print("[Off] \(message)")
-            case .all: print("[All] \(message)")
-            }
+        let realm = try! Realm()
+        try! realm.write {
+            realm.deleteAll()
+            
+            realm.add(TextRealmModel())
         }
         
-        app.syncManager.logLevel = .all
+        let obj: TextRealmModel = realm.objects(TextRealmModel.self).first!
+        let duplicate = obj.duplicate()
         
-        app.login(credentials: .anonymous) { [self] result in
-            switch result {
-            case .failure(let error):
-                print("Realm login failed: \(error)")
-                
-            case .success(let user):
-                print("Realm login success with user ID '\(user.id)'")
-                
-                var configuration = user.configuration(partitionValue: "P")
-                configuration.objectTypes = [
-                    TextRealmModel.self
-                ]
-                
-                Realm.asyncOpen(configuration: configuration, callbackQueue: realmQueue) { result in
-                    switch result {
-                    case .failure(let error):
-                        print("Failed to open realm: \(error)")
-                        
-                    case .success(let realm):
-                        print("Realm async open success")
-                        self.realm = realm
-                        
-                        realmQueue.async { [self] in
-                            token = realm.objects(TextRealmModel.self).observe(on: realmQueue, { changeset in
-                                switch changeset {
-                                    
-                                case .initial(let results):
-                                    let text = results.first!.text
-                                    print("Initial text set: \(text)")
-                                    DispatchQueue.main.async {
-                                        textLabel.text = "Local: \(text)"
-                                        realmLabel.text = "Realm: \(text)"
-                                    }
-                                    
-                                case .update(let results, _, _, _):
-                                    let text = results.first!.text
-                                    print("Update text set: \(text)")
-                                    DispatchQueue.main.async {
-                                        realmLabel.text = "Realm: \(text)"
-                                    }
-                                    
-                                case .error(let error):
-                                    print("Realm observe error: \(error)")
-                                }
-                            })
-                        }
-                    }
-                }
-            }
+        duplicate.text = "asd"
+        try! realm.write {
+            realm.add(duplicate)
         }
-    }
-    
-    @IBAction fileprivate func onGenerateTap(_ sender: Any) {
-        let text = String.random(length: 10)
-        textLabel.text = "Local: \(text)"
-        let value = AnyBSON(text)
         
-        app.currentUser?.functions.updateTexts([value]) { result, error in
-            if let error = error {
-                print("Function call error: \(error)")
-            } else {
-                print("Function call success")
-            }
-        }
-    }
-    
-    @IBAction fileprivate func onForceRefreshTap(_ sender: Any) {
-        realmQueue.async { [self] in
-            realm.refresh()
-        }
+        let objs = realm.objects(TextRealmModel.self)
+        
     }
 }
 
@@ -125,4 +50,118 @@ final class TextRealmModel: Object {
     @objc dynamic var text: String = ""
     
     override static func primaryKey() -> String? { "_id" }
+}
+
+protocol DetachableObject: AnyObject {
+    func detached() -> Self
+    func duplicate() -> Self
+}
+
+protocol RealmDetachableObject: DetachableObject & NSObject {
+    var objectSchema: RealmSwift.ObjectSchema { get }
+}
+
+extension RealmDetachableObject {
+    /// Returns unmanaged object
+    func detached() -> Self {
+        let detached = type(of: self).init()
+        for property in objectSchema.properties {
+            guard let value = value(forKey: property.name) else { continue }
+            if property.isArray == true {
+                // Realm List property support
+                if let detachable = value as? DetachableObject {
+                    detached.setValue(detachable.detached(), forKey: property.name)
+                } else {
+                    // logError
+                }
+                
+            } else if property.type == .object {
+                // Realm Object property support
+                if let detachable = value as? DetachableObject {
+                    detached.setValue(detachable.detached(), forKey: property.name)
+                } else {
+                    // logError
+                }
+                
+            } else {
+                detached.setValue(value, forKey: property.name)
+            }
+        }
+        return detached
+    }
+    
+    func duplicate() -> Self {
+        let selfType = type(of: self)
+        let duplicate = selfType.init()
+        let primaryKey = (selfType as? Object.Type)?.primaryKey()
+        for property in objectSchema.properties {
+            guard let value = value(forKey: property.name) else { continue }
+            if property.isArray == true {
+                // Realm List property support
+                if let detachable = value as? DetachableObject {
+                    duplicate.setValue(detachable.duplicate(), forKey: property.name)
+                } else {
+                    // logError
+                    print("error")
+                }
+                
+            } else if property.type == .object {
+                // Realm Object property support
+                if let detachable = value as? DetachableObject {
+                    duplicate.setValue(detachable.duplicate(), forKey: property.name)
+                } else {
+                    // logError
+                    print("error")
+                }
+                
+            } else {
+                // Primary key change
+                if property.name == primaryKey {
+                    if value is ObjectId {
+                        duplicate.setValue(ObjectId.generate(), forKey: property.name)
+                    } else if value is String {
+                        duplicate.setValue(UUID().uuidString, forKey: property.name)
+                    } else {
+                        // logError
+                        print("error")
+                    }
+                    
+                } else {
+                    duplicate.setValue(value, forKey: property.name)
+                }
+            }
+        }
+        return duplicate
+    }
+}
+
+extension Object: RealmDetachableObject {}
+extension EmbeddedObject: RealmDetachableObject {}
+
+extension List: DetachableObject {
+    
+    /// Returns list of detached objects
+    func detached() -> Self {
+        let result = Self()
+        
+        forEach {
+            if let detachable = $0 as? DetachableObject {
+                let detached = detachable.detached() as! Element
+                result.append(detached)
+            } else {
+                result.append($0) // Primtives are pass by value; don't need to recreate
+            }
+        }
+        
+        return result
+    }
+    
+    func duplicate() -> Self {
+        return detached()
+    }
+    
+    /// Returns array of detached objects
+    func toArray() -> [Element] {
+        Array(detached())
+    }
 }
