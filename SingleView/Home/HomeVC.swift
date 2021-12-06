971ee4e63dce93d8b204f006f6d435e568f36a62
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-12-06 11:37:04 +0300

    - Observe Realm objects on a queue check

--- SingleView/Home/HomeVC.swift
+++ SingleView/Home/HomeVC.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Anton Plebanovich. All rights reserved.
 //
 
+import RealmSwift
 import UIKit
 
 final class HomeVC: UIViewController {
@@ -13,6 +14,9 @@ final class HomeVC: UIViewController {
     // ******************************* MARK: - @IBOutlets
     
     // ******************************* MARK: - Private Properties
+    
+    let queue = DispatchQueue(label: "HomeVC", qos: .background)
+    var token: NotificationToken!
 
     // ******************************* MARK: - Initialization and Setup
     
@@ -26,5 +30,36 @@ final class HomeVC: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        queue.async { [self] in
+            let realm = try! Realm(configuration: .defaultConfiguration, queue: queue)
+            token = realm.objects(MyObject.self).observe(on: queue, { result in
+                switch result {
+                case .initial: break
+                case .update(_, let deletions, let insertions, let modifications):
+                    print("\(deletions)-\(insertions)-\(modifications)")
+                    
+                case .error(let error):
+                    fatalError("\(error)")
+                }
+            })
+            
+            queue.async {
+                let realm = try! Realm(configuration: .defaultConfiguration, queue: queue)
+                try! realm.write {
+                    realm.add(MyObject())
+                }
+            }
+        }
     }
+    
+    @IBAction fileprivate func onTap(_ sender: Any) {
+        let realm = try! Realm(configuration: .defaultConfiguration)
+        try! realm.write {
+            realm.add(MyObject())
+        }
+    }
+}
+
+final class MyObject: Object {
+    @objc dynamic var message: String = ""
 }
