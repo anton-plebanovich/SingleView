721f5a9207396f35691a49eab6c36f63f00105cd
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-06-16 23:26:34 +0300

    - Bug reproduction

--- SingleView/Home/HomeVC.swift
+++ SingleView/Home/HomeVC.swift
@@ -14,15 +14,9 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
     
     // ******************************* MARK: - @IBOutlets
     
-    @IBOutlet fileprivate var textLabel: UILabel!
-    @IBOutlet fileprivate var realmLabel: UILabel!
-    
     // ******************************* MARK: - Private Properties
     
-    fileprivate let realmQueue = DispatchQueue(label: "realm", qos: .userInteractive)
-    fileprivate let computeQueue = DispatchQueue(label: "compute", qos: .userInteractive)
     fileprivate let app = App(id: "realmsandbox-grpmo")
-    fileprivate var realm: Realm!
     fileprivate var token: NotificationToken!
     
     // ******************************* MARK: - Initialization and Setup
@@ -30,6 +24,22 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupApp(app: app)
+        
+        // Please set breakpoint to RLMSyncSession.mm:64 to be able to investigate app state
+        login(app: app) { [self] user in
+            openRealm(user: user) { realm in
+                // Capture token and release `realm`
+                let token = realm.syncSession?.addProgressNotification(for: .download, mode: .reportIndefinitely) { _ in }
+                DispatchQueue.main.async {
+                    // Invalidate and release `token` to observe error message
+                    token?.invalidate()
+                }
+            }
+        }
+    }
+    
+    fileprivate func setupApp(app: App) {
         app.syncManager.logger = { logLevel, message in
             switch logLevel {
             case .fatal: print("SyncManager fatal error: \(message)")
@@ -45,76 +55,38 @@ final class HomeVC: UIViewController, InstantiatableFromStoryboard {
         }
         
         app.syncManager.logLevel = .all
-        
-        app.login(credentials: .anonymous) { [self] result in
+    }
+    
+    fileprivate func login(app: App, completion: @escaping (User) -> Void) {
+        app.login(credentials: .anonymous) { result in
             switch result {
             case .failure(let error):
-                print("Realm login failed: \(error)")
+                fatalError("Realm login failed: \(error)")
                 
             case .success(let user):
                 print("Realm login success with user ID '\(user.id)'")
-                
-                var configuration = user.configuration(partitionValue: "P")
-                configuration.objectTypes = [
-                    TextRealmModel.self
-                ]
-                
-                Realm.asyncOpen(configuration: configuration, callbackQueue: realmQueue) { result in
-                    switch result {
-                    case .failure(let error):
-                        print("Failed to open realm: \(error)")
-                        
-                    case .success(let realm):
-                        print("Realm async open success")
-                        self.realm = realm
-                        
-                        realmQueue.async { [self] in
-                            token = realm.objects(TextRealmModel.self).observe(on: realmQueue, { changeset in
-                                switch changeset {
-                                    
-                                case .initial(let results):
-                                    let text = results.first?.text
-                                    print("Initial text set: \(text ?? "nil")")
-                                    DispatchQueue.main.async {
-                                        textLabel.text = "Local: \(text ?? "nil")"
-                                        realmLabel.text = "Realm: \(text ?? "nil")"
-                                    }
-                                    
-                                case .update(let results, _, _, _):
-                                    let text = results.first?.text
-                                    print("Update text set: \(text ?? "nil")")
-                                    DispatchQueue.main.async {
-                                        realmLabel.text = "Realm: \(text ?? "nil")"
-                                    }
-                                    
-                                case .error(let error):
-                                    print("Realm observe error: \(error)")
-                                }
-                            })
-                        }
-                    }
-                }
+                completion(user)
             }
         }
     }
     
-    @IBAction fileprivate func onGenerateTap(_ sender: Any) {
-        let text = String.random(length: 10)
-        textLabel.text = "Local: \(text)"
-        let value = AnyBSON(text)
+    fileprivate func openRealm(user: User, completion: @escaping (Realm) -> Void) {
+        var configuration = user.configuration(partitionValue: "P")
+        configuration.objectTypes = [
+            TextRealmModel.self
+        ]
         
-        app.currentUser?.functions.updateTexts([value]) { result, error in
-            if let error = error {
-                print("Function call error: \(error)")
-            } else {
-                print("Function call success")
+        Realm.asyncOpen(configuration: configuration) { result in
+            switch result {
+            case .failure(let error):
+                print("Failed to open realm: \(error)")
+                _ = try! Realm.deleteFiles(for: configuration)
+                self.openRealm(user: user, completion: completion)
+                
+            case .success(let realm):
+                print("Realm async open success")
+                completion(realm)
             }
-        }
-    }
-    
-    @IBAction fileprivate func onForceRefreshTap(_ sender: Any) {
-        realmQueue.async { [self] in
-            realm.refresh()
         }
     }
 }
